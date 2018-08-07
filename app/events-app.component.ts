401ebe1a78664496daf72daff38ed3ff1f678f07
nucknine <nucknine@gmail.com>
2018-08-07 16:10:40 +0300

    start

--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core'
 
-@Component(
-    {
-        selector: 'events-app',
-        template: '<h2>Hello world</h2>'
-    }
-)
+@Component({
+  selector: 'events-app',
+  template: '<h2>Hello World</h2>'
+})
 export class EventsAppComponent {
 
 }
